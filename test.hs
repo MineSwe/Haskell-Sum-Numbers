90fe428fa3d7f1ea8631bdabe62999616c467dbb
msorberg <msorberg@kth.se>
2025-09-09 17:35:56 +0200

    Fixes #2

--- test.hs
+++ test.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 getAverageOfTopHalf :: Int -> [Int] -> Int
 getAverageOfTopHalf listLength list =
     let sortedList = sortList list
@@ -6,6 +8,10 @@ getAverageOfTopHalf listLength list =
     in averageOfTopHalf
 
 -- Sort list
+sortList :: [Int] -> [Int]
+sortList list =
+    let sortedList = sort list
+    in sortedList
 
 -- Get top half by listLength / 2
 
